495b4cefaa0c498e20ec974b9bc583d2041f04a0
hedleyroos <hedleyroos@gmail.com>
2016-08-25 17:30:28 +0200

    More tests

--- listing/tests/test_base.py
+++ listing/tests/test_base.py
@@ -4,6 +4,7 @@ import unittest
 from django.core.urlresolvers import reverse
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.sites.models import Site
 from django.test.client import Client, RequestFactory
 
 from listing.models import Listing
@@ -45,11 +46,74 @@ class BaseTestCase(unittest.TestCase):
         obj = ModelB.objects.create(title="ModelB", slug="model-b")
         cls.model_b = obj
 
+        obj = ModelA.objects.create(title="ModelA Published", slug="model-a-p")
+        obj.sites = Site.objects.all()
+        obj.publish()
+        cls.model_a_published = obj
+
+        obj = ModelB.objects.create(title="ModelB Published", slug="model-b-p")
+        obj.sites = Site.objects.all()
+        obj.publish()
+        cls.model_b_published = obj
+
     def test_content_types(self):
         listing = Listing.objects.create()
         listing.content_types = [ContentType.objects.get_for_model(ModelA)]
         listing.save()
         qs = listing.queryset
-        self.assertEqual(len(qs), 1)
+        self.assertEqual(len(qs), 2)
         self.failUnless(self.model_a.modelbase_obj in qs)
+
+    def test_content_types_permitted(self):
+        listing = Listing.objects.create()
+        listing.content_types = [ContentType.objects.get_for_model(ModelA)]
+        listing.save()
+        qs = listing.queryset_permitted
+        self.assertEqual(len(qs), 1)
+        self.failUnless(self.model_a_published.modelbase_obj in qs)
+
+    def test_content(self):
+        listing = Listing.objects.create()
+        listing.set_content([self.model_a, self.model_a_published])
+        qs = listing.queryset
+        self.assertEqual(len(qs), 2)
+        self.failUnless(self.model_a.modelbase_obj in qs)
+        qs = listing.content_queryset
+        self.assertEqual(len(qs), 2)
+        self.failUnless(self.model_a.modelbase_obj in qs)
+
+    def test_content_permitted(self):
+        listing = Listing.objects.create()
+        listing.set_content([self.model_a, self.model_a_published])
+        qs = listing.queryset_permitted
+        self.assertEqual(len(qs), 1)
+        self.failUnless(self.model_a_published.modelbase_obj in qs)
+        qs = listing.content_queryset_permitted
+        self.assertEqual(len(qs), 1)
+        self.failUnless(self.model_a_published.modelbase_obj in qs)
+
+    def test_pinned(self):
+        listing = Listing.objects.create()
+        listing.set_pinned([self.model_a, self.model_a_published])
+        qs = listing.pinned_queryset
+        self.assertEqual(len(qs), 2)
+        self.failUnless(self.model_a.modelbase_obj in qs)
+
+    def test_pinned_permitted(self):
+        listing = Listing.objects.create()
+        listing.set_pinned([self.model_a, self.model_a_published])
+        qs = listing.pinned_queryset_permitted
+        self.assertEqual(len(qs), 1)
+        self.failUnless(self.model_a_published.modelbase_obj in qs)
+
+    def test_content_with_pinned(self):
+        """Pinned content is excluded from queryset"""
+        listing = Listing.objects.create()
+        listing.set_content([self.model_a, self.model_a_published])
+        listing.set_pinned([self.model_a])
+        qs = listing.queryset
+        self.assertEqual(len(qs), 1)
+        self.failIf(self.model_a.modelbase_obj in qs)
+
+
 
